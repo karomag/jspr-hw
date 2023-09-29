8ab5a2bd3ccfde55a25a7924da9315312efee7ad
karomag <karomag88@mail.ru>
2023-09-30 01:07:18 +0300

    add handlers

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,8 +1,46 @@
 package ru.netology;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(64);
+
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                try {
+                    responseStream.write((
+                            "HTTP/1.1 404 Not Found\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    responseStream.flush();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                try {
+                    server.responseBadRequest(responseStream);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        server.addHandler("GET", "/", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                try {
+                    server.responseDefault(responseStream, "/index.html");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
         server.listen(9999);
     }
 }
